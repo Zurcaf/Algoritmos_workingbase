8a38275cbbcdcdd0493c5b8a41adaeba98389524
zurcaf <afonsobpcruz@gmail.com>
2022-10-11 15:28:21 +0100

    .pals e .dict a serem confirmados

--- 2.ProjetoAED/0-wordsMutation.c
+++ 2.ProjetoAED/0-wordsMutation.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "headers/0-wordsMutation.h"
 #include "headers/1-dicionario.h"
 #include "headers/2-pals.h"
 
+#define DICT_EXT ".dict"
+#define PALS_EXT ".pals"
+
 int main(int argc, char* argv[])
 {
 argsCheck(argc);
 
-char* dictLocation;
-char* palsLocation;
+//mudar para wrdmttns no make file
+
+// char locationDict[] = "ficheirosTeste/dicionarios/portugues04-04-sl.dict";
+//char locationDict[] = "ficheirosTeste/dicionarios/testarContagem.dict";
+// char locationDict[] = "ficheirosTeste/dicionarios/portugues04-08.dict";
+// char locationDict[] = "ficheirosTeste/dicionarios/portugues04-04-sl.dict";
+//char* dictLocation = "ficheirosTeste/dicionarios/testarContagem.dict";
+char* dictLocation = "ficheirosTeste/dicionarios/portugues04-08.dict";
+char* palsLocation = "ficheirosTeste/dicionarios/Simple01.pals";
+bool* palsActivation = NULL;
 
 dictAndPalsCheck(argv, dictLocation, palsLocation);
-saveDictionary(dictLocation);
-savePals(palsLocation);
-
+savePals(palsLocation, palsActivation);
+saveDictionary(dictLocation, palsActivation);
 
 
 
@@ -32,7 +43,37 @@ void argsCheck(int argc)
     }
 }
 
-void dictAndPalsCheck(char* argv[], char* dictLocation, char* palsLocation)
+void dictAndPalsCheck(char *argv[], char *dictLocation, char *palsLocation)
 {
+    int lenArgs[2], auxLen[2], i=0, j=0;
+    char dict[] = DICT_EXT, pals[] = PALS_EXT;
 
+    lenArgs[0] = strlen(argv[1]);
+    lenArgs[1] = strlen(argv[2]);
+    
+    auxLen[0] = lenArgs[0] - (strlen (DICT_EXT));
+    auxLen[1] = lenArgs[1] - (strlen (PALS_EXT));
+
+    for(i=0; i<2; i++)
+    {
+        for(j=auxLen[i]; j<lenArgs[i]; j++)
+        {
+            if(i==0)
+            {
+                if(argv[1][j] != dict[j-auxLen[i]])
+                {
+                    fprintf(stderr, "ERROR: wrong dictionary extension!\n");
+                    exit(2);
+                }
+            }
+            else
+            {
+                if(argv[2][j] != pals[j-auxLen[i]])
+                {
+                    fprintf(stderr, "ERROR: wrong pals extension!\n");
+                    exit(3);
+                }
+            }
+        }
+    }
 }
--- 2.ProjetoAED/2-pals.c
+++ 2.ProjetoAED/2-pals.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "headers/2-pals.h"
 
-
-
-
-void savePals(char* palsLocation)
+void savePals(char* palsLocation, bool* palsActivation)
 {
 
 }
